65ab4c362c41825a27702ffd472066cc79f588bb
andy <andygrayia@gmail.com>
2016-10-25 10:10:20 -0400

    CHALLENGE_FLOW: added new message type

--- chally_flow/nodes/flow.py
+++ chally_flow/nodes/flow.py
@@ -46,78 +46,8 @@ class flow(object):
 		self.deliver_shape_4 = rospy.get_param('~deliver_shape_4', 'none')
 
 
-		#need a while loop that waits for a query of the next task
-
-		self.base()
-		rospy.Subscriber("joy", Joy, self.action)
-		rospy.Subscriber("/odometry/filtered", Odometry, self.odom_location)
-		self.pose_pub = rospy.Publisher("RC_position", PoseWithCovarianceStamped, queue_size = 1)
-
-
-
-		rate = rospy.Rate(20)
-		self.start()
-
-		while not rospy.is_shutdown():
-
-			br = tf2_ros.TransformBroadcaster()
-			t = geometry_msgs.msg.TransformStamped()
-
-			self.subposx += self.movex
-			self.subposy += self.movey
-
-			quat = (
-				self.subrotx,
-				self.subroty,
-				self.subrotz,
-				self.subrotw)
-
-			euler = tf.transformations.euler_from_quaternion(quat)
-			
-			turn = euler[2] + self.yaw
-			euler = (
-				euler[0],
-				euler[1],
-				turn)
-			#rospy.loginfo(euler)
-
-			quat = tf.transformations.quaternion_from_euler(euler[0],euler[1], euler[2])
-			self.subrotx = quat[0]
-			self.subroty = quat[1]
-			self.subrotz = quat[2]
-			self.subrotw = quat[3]
-
-			t.transform.translation.x = self.subposx
-			t.transform.translation.y = self.subposy
-			t.transform.translation.z = self.subposz
-			t.transform.rotation.x = self.subrotx
-			t.transform.rotation.y = self.subroty
-			t.transform.rotation.z = self.subrotz
-			t.transform.rotation.w = self.subrotw
-
-			#odom = Odometry()
-
-			t.header.stamp = rospy.Time.now()
-			t.header.frame_id = "map"
-			t.child_frame_id = "desired_position"
-			br.sendTransform(t)	
-
-			'''t.transform.translation.x = 0
-			t.transform.translation.y = 0
-			t.transform.translation.z = 0
-			t.transform.rotation.x = 0
-			t.transform.rotation.y = 0
-			t.transform.rotation.z = 0
-			t.transform.rotation.w = 1.0
-
-			#odom = Odometry()
-
-			t.header.stamp = rospy.Time.now()
-			t.header.frame_id = "map"
-			t.child_frame_id = "odom"
-			br.sendTransform(t)	'''
-
-			rate.sleep()
+		#need a while loop that waits for a query of the task info
+		#publish shooter solution, docking solution.....
 
 def main():
 	rospy.init_node('challenge_flow', anonymous=False)
